cb6a745b4fe8ac082fd0a49ab571335150dfc30e
luancaius <luan.ca@outlook.com>
2023-01-14 19:45:00 -0500

    initial files

---
+++ memev2/memev2/EditMemeViewController.swift
@@ -0,0 +1,162 @@
+//
+//  ViewController.swift
+//  memev2
+//
+//  Created by Luan Ramos on 2022-12-29.
+//
+
+import UIKit
+
+class EditMemeViewController: UIViewController, UIImagePickerControllerDelegate,
+                      UINavigationControllerDelegate, UITextFieldDelegate {
+    
+    @IBOutlet weak var bottomText: UITextField!
+    
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var imagePickerView: UIImageView!
+    @IBOutlet weak var topText: UITextField!
+    
+    @IBOutlet weak var shareButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        shareButton.isEnabled = false
+        prepareTextField(textField: topText, defaultText: "TOP")
+        prepareTextField(textField: bottomText, defaultText: "BOTTOM")
+#if targetEnvironment(simulator)
+    cameraButton.isEnabled = false;
+#else
+    cameraButton.isEnabled = UIImagePickerController.isSourceTypeAvailable(.camera);
+#endif
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        super.viewWillAppear(animated)
+        subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        
+        super.viewWillDisappear(animated)
+        unsubscribeFromKeyboardNotifications()
+    }
+    
+    func prepareTextField(textField: UITextField, defaultText: String) {
+        let memeTextAttributes: [NSAttributedString.Key: Any] = [
+            NSAttributedString.Key.strokeColor: UIColor.black,
+            NSAttributedString.Key.foregroundColor: UIColor.white,
+            NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSAttributedString.Key.strokeWidth:  -3.5
+        ]
+        textField.defaultTextAttributes = memeTextAttributes
+        textField.text = defaultText
+        textField.textAlignment = .center
+        textField.delegate = self
+        textField.adjustsFontSizeToFitWidth = true
+        textField.minimumFontSize = 12.0
+    }
+    
+    @IBAction func reset(_ sender: Any) {
+        topText.text = ""
+        bottomText.text = ""
+        self.imagePickerView.image = nil
+    }
+    func subscribeToKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    func unsubscribeFromKeyboardNotifications() {
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(_ notification:Notification) {
+        if bottomText!.isEditing {
+            view.frame.origin.y = -getKeyboardHeight(notification)
+        }
+    }
+    
+    @objc func keyboardWillHide(_ notification:Notification) {
+        view.frame.origin.y = 0
+    }
+    
+    func getKeyboardHeight(_ notification:Notification) -> CGFloat {
+        
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIResponder.keyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.cgRectValue.height
+    }
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        if textField.tag == 0 {
+            topText.text = ""
+        }
+        if textField.tag == 1 {
+            bottomText.text = ""
+        }
+    }
+   
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @IBAction func pickAnImage(_ sender: Any) {
+        pickImage(sourceType: .photoLibrary)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imagePickerView.image = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        self.dismiss(animated: true, completion: nil)
+        repositionTexts()
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func pickImage(sourceType: UIImagePickerController.SourceType){
+        let imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+        imagePicker.sourceType = sourceType
+        present(imagePicker, animated: true, completion: nil)
+        shareButton.isEnabled = true
+    }
+    
+    @IBAction func takePicture(_ sender: Any) {
+        pickImage(sourceType: .camera)
+    }
+    
+    @IBAction func shareMeme(_ sender: Any) {
+        let memedImage = generateMemedImage()
+        let shareController = UIActivityViewController(activityItems: [memedImage], applicationActivities: nil)
+        
+        shareController.completionWithItemsHandler = { (activityType, completed, returnedItems, activityError) -> () in
+            if (completed) {
+                self.saveMeme(memedImage)
+                self.dismiss(animated: true, completion: nil)
+            }
+        }
+        present(shareController, animated: true, completion: nil)
+    }
+    
+    func saveMeme(_ memedImage: UIImage) {
+        
+        var memedImage = generateMemedImage()
+        let meme = Meme(topText: topText.text!, bottomText: bottomText.text!, originalImage: imagePickerView.image!, memedImage: memedImage)
+    }
+    
+    func generateMemedImage() -> UIImage {
+        UIGraphicsBeginImageContext(self.view.frame.size)
+        view.drawHierarchy(in: self.view.frame, afterScreenUpdates: true)
+        let memedImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+                
+        return memedImage
+    }
+    
+    func repositionTexts() {
+        topText.frame = CGRectMake(imagePickerView.bounds.origin.x, imagePickerView.frame.origin.y - topText.frame.size.height, imagePickerView.frame.size.width, topText.frame.size.height);
+    }
+}
+
---
+++ memev2/memev2/MemeModel.swift
@@ -0,0 +1,18 @@
+//
+//  MemeModel.swift
+//  memev2
+//
+//  Created by Luan Ramos on 2023-01-08.
+//
+
+import Foundation
+import UIKit
+
+
+struct Meme {
+    var topText: String
+    var bottomText: String
+    var originalImage: UIImage
+    var memedImage: UIImage
+    
+}
